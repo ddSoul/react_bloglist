d4b1725f1ddc2102c5f92653af9867cadbf3cc55
邓西亮 <dengxlc@yonyou.com>
2021-06-24 16:50:47 +0800

    bloglist init

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                首页 博客列表
+            </div>
+        );
+    }
+}
+
+export default Home;
